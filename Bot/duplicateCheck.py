c25a3acfb92f375b48e68e00d6af2c23a0b1d4cc
raghavTinker <raghav_tinker@yahoo.com>
2021-07-01 10:41:46 +0530

    Feature Added: Prevention of duplication of URLs

---
+++ Bot/duplicateCheck.py
@@ -0,0 +1,27 @@
+import requests
+import json
+import os
+
+database = os.environ["DATABASE_TOKEN"]
+print(database)
+url = "https://api.notion.com/v1/databases/" + str(database) + "/query"
+
+def doesItExist(link):
+  payload = json.dumps({
+    "filter": {
+      "property": "URL",
+      "url":{
+        "equals": link
+      }
+    }
+  })
+  headers = {
+    'Authorization': str(os.environ["AUTH_KEY"]),
+    'Notion-Version': '2021-05-13',
+    'Content-Type': 'application/json'
+  }
+  response = requests.post(url, headers=headers, data=payload)
+  result = response.json()["results"]
+  if(len(result) == 0):
+    return False
+  return True
