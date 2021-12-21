a10f0a51fa210e62f4a8f711a1d4b510e6a95c6d
Raghav Sharma <raghav_tinker@yahoo.com>
2021-12-22 00:47:12 +0530

    delete by title made

--- botV2/deleteRecord.py
+++ botV2/deleteRecord.py
@@ -2,6 +2,9 @@ import requests
 import json
 import os
 from utils import *
+import discord
+from discord.ext import commands
+import asyncio
 
 def patch(notion_key, payload, searchObj_toDelete):
     headers = {
@@ -125,3 +128,50 @@ def deleteAll(searchObj_toDelete, api_key):
         }
     })
     patch(api_key, payload, searchObj_toDelete)
+
+# bot delete by title function
+async def delByTitle(ctx, query, client, bot):
+    search_results = searchByTitle(query.strip(), client.notion_db_id, client.notion_api_key)
+    
+    # check if there are results
+    print(len(search_results))
+    if len(search_results) == 0:
+        await ctx.send("No results found")
+        return
+    count = 1    
+    
+    embed = discord.Embed(title="Type in the serial number you want to delete", description="Results for {}".format(query), color=discord.Color.green())
+    for result in search_results:
+        # make sure title and url are present
+        if result.title.strip() == "" and result.url == None:
+            continue
+        embed.add_field(name=str(count)+". "+ result.title.strip(), value=result.url, inline=False)
+        count += 1
+    await ctx.send(embed=embed)
+
+    # get users response for index
+    def check(reply_user):
+        return reply_user.author == ctx.author and reply_user.channel == ctx.channel
+    # timeout error
+    try:
+        msg = await bot.wait_for("message", check=check, timeout=60)
+    except asyncio.TimeoutError:
+        embed = discord.Embed(title="No response", description=f"Waited for 60s no response received", color=discord.Color.red())
+        await ctx.send("You have not responded for 60s so quitting!")
+        return
+    # Check if the input is valid
+    try:
+        option_to_delete = int(msg.content)
+        title = search_results[option_to_delete-1].title
+        print(title)
+        #deleteMe(search_results[option_to_delete-1])
+        embed = discord.Embed(title="Successful! Record deleted", description=f"{title} deleted!", color=discord.Color.green())
+        await ctx.send(embed=embed)
+    except:
+        embed = discord.Embed(title="Enter Valid input", description="This option doesn't exist", color=discord.Color.red())
+        await ctx.send(embed=embed)
+
+# bot delete by tag function
+async def delByTag(ctx, query, client, bot):
+    # TODO: rdotjain - add tag deletion
+    print("deleted by tag")
