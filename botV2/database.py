f2258c501a6a3fa05db8d0bc512bef076c0a8597
Raghav Sharma <raghav_tinker@yahoo.com>
2021-12-31 21:21:42 +0530

    making database docker ready

--- botV2/database.py
+++ botV2/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///clients.sqlite'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///database/clients.sqlite'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URI, connect_args={'check_same_thread': False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
