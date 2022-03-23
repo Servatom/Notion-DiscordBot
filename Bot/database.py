9155518f418a142f1ce8169312d4dc9159c1999f
Raghav Sharma <raghav_tinker@yahoo.com>
2022-03-24 01:15:44 +0530

    added postgressql support

--- Bot/database.py
+++ Bot/database.py
@@ -1,10 +1,17 @@
 from sqlalchemy import create_engine
+import sqlalchemy
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///database/clients.sqlite'
+url = os.environ.get('DATABASE_URL')
+if url and url.startswith("postgres://"):
+    url = url.replace("postgres://", "postgresql://", 1)
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///form.sqlite'
+SQLALCHEMY_DATABASE_URI = url
 
-engine = create_engine(SQLALCHEMY_DATABASE_URI, connect_args={'check_same_thread': False})
+engine = create_engine(SQLALCHEMY_DATABASE_URI)
+#engine = create_engine(SQLALCHEMY_DATABASE_URI, connect_args={'check_same_thread': True},pool_pre_ping=True,pool_recycle=300)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
